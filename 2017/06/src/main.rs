bb745d4ae593630278cd49e45ff329b44fab5d0d
Sebastian Nowicki <sebnow@gmail.com>
2017-12-06 18:17:25 +0100

    2017-06: Add solution for part two

--- 2017/06/src/main.rs
+++ 2017/06/src/main.rs
@@ -1,14 +1,29 @@
 mod bank;
 use bank::Bank;
 
-fn redistributions(bank: Bank) -> i32 {
+#[derive(Debug)]
+struct Res {
+    redis_cycles: i32,
+    cycles: i32,
+}
+
+fn pos(banks: &Vec<Bank>, bank: &Bank) -> Option<usize> {
+    banks.iter().find(|&b| b == bank).and_then(|other| {
+        banks.iter().position(|x| x == other)
+    })
+}
+
+fn redistributions(bank: Bank) -> Res {
     let mut banks = vec![bank];
 
     loop {
         let new_bank = banks.last().unwrap().redistribute();
 
-        if banks.iter().find(|b| *b == &new_bank).is_some() {
-            return banks.len() as i32;
+        if let Some(p) = pos(&banks, &new_bank) {
+            return Res {
+                redis_cycles: banks.len() as i32,
+                cycles: (banks.len() - p) as i32,
+            };
         }
 
         banks.push(new_bank);
@@ -17,7 +32,7 @@ fn redistributions(bank: Bank) -> i32 {
 
 fn main() {
     println!(
-        "{0}",
+        "{:?}",
         redistributions(Bank::new(
             [5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6],
         ))
