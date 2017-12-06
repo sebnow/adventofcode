a2e09f026ee4fb08ffdc5d0af176d47ab8f2d800
Sebastian Nowicki <sebnow@gmail.com>
2017-12-06 19:47:46 +0100

    2017-05: Add test case for example input

--- 2017/05/src/main.rs
+++ 2017/05/src/main.rs
@@ -1,12 +1,27 @@
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Answer {
     steps: i32,
 }
 
-fn answer() -> Answer {
+fn answer_with_input(input: &[i32]) -> Answer {
     Answer{steps: 0}
+}
+
+fn answer() -> Answer {
+    let input = [];
+    answer_with_input(&input)
 }
 
 fn main() {
     println!("{:?}", answer());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(answer_with_input(&[0, 3, 0, 1, -3]), Answer{steps: 5});
+    }
 }
