99d5552b31110061b41642a573811411e156e38a
Sebastian Nowicki <sebnow@gmail.com>
2017-12-06 20:11:02 +0100

    2017-05: Solve for example input

--- 2017/05/src/main.rs
+++ 2017/05/src/main.rs
@@ -3,13 +3,27 @@ struct Answer {
     steps: i32,
 }
 
-fn answer_with_input(input: &[i32]) -> Answer {
-    Answer{steps: 0}
+fn answer_with_input(input: &mut [i32]) -> Answer {
+    let length = input.len();
+
+    let mut steps = 0;
+    let mut pos: i32 = 0;
+
+    while 0 <= pos && pos < (length as i32) {
+        let idx = pos as usize;
+        let jump = input[idx];
+
+        input[idx] += 1;
+        pos += jump;
+        steps += 1;
+    }
+
+    Answer{steps: steps}
 }
 
 fn answer() -> Answer {
-    let input = [];
-    answer_with_input(&input)
+    let mut input = [];
+    answer_with_input(&mut input)
 }
 
 fn main() {
@@ -22,6 +36,7 @@ mod tests {
 
     #[test]
     fn example() {
-        assert_eq!(answer_with_input(&[0, 3, 0, 1, -3]), Answer{steps: 5});
+        let mut input = [0, 3, 0, 1, -3];
+        assert_eq!(answer_with_input(&mut input), Answer{steps: 5});
     }
 }
