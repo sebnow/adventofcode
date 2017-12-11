6fce0e3dc08ccb3a4870004df896b6229fb95852
Sebastian Nowicki <sebnow@gmail.com>
2017-12-11 20:02:07 +0100

    2017-02: Solve for part 1

--- 2017/02/src/main.rs
+++ 2017/02/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn answer_1<R: BufRead>(reader: R) -> i32 {
+    reader.lines()
+        .map(|l| {
+            let xs: Vec<i32> = l.unwrap()
+                .split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect();
+            xs.iter().max().unwrap() - xs.iter().min().unwrap()
+        })
+        .sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    println!("Part 1: {:?}", answer_1(reader));
+    println!("Part 2: ?");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn example_answer_1() {
+        assert_eq!(answer_1(Cursor::new("5 1 9 5\n7 5 3\n2 4 6 8")), 18);
+    }
 }
