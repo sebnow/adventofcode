d637aca08cb675746a950aa43042a6b3f02896e1
Sebastian Nowicki <sebnow@gmail.com>
2017-12-11 22:26:30 +0100

    2017-02: Solve for part 2

--- 2017/02/src/main.rs
+++ 2017/02/src/main.rs
@@ -1,14 +1,34 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn answer_1<R: BufRead>(reader: R) -> i32 {
-    reader.lines()
-        .map(|l| {
-            let xs: Vec<i32> = l.unwrap()
-                .split_whitespace()
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect();
-            xs.iter().max().unwrap() - xs.iter().min().unwrap()
+fn parse_line(l: String) -> Vec<i32> {
+    l.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect()
+}
+
+fn parse_input<R: BufRead>(reader: R) -> Vec<Vec<i32>> {
+    reader.lines().map(|l| parse_line(l.unwrap())).collect()
+}
+
+fn answer_1(input: &Vec<Vec<i32>>) -> i32 {
+    input.iter().map(|xs| xs.iter().max().unwrap() - xs.iter().min().unwrap()).sum()
+}
+
+fn answer_2(input: &Vec<Vec<i32>>) -> i32 {
+    input
+        .iter()
+        .map(|xs| {
+            for &i in xs.iter() {
+                for &j in xs.iter() {
+                    if i == j {
+                        continue
+                    }
+                    if i as f32 % j as f32 == 0.0 {
+                        return i / j
+                    }
+                }
+            }
+
+            0
         })
         .sum()
 }
@@ -16,9 +36,10 @@ fn answer_1<R: BufRead>(reader: R) -> i32 {
 fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
+    let xs = parse_input(reader);
 
-    println!("Part 1: {:?}", answer_1(reader));
-    println!("Part 2: ?");
+    println!("Part 1: {:?}", answer_1(&xs));
+    println!("Part 2: {:?}", answer_2(&xs));
 }
 
 #[cfg(test)]
@@ -28,6 +49,11 @@ mod test {
 
     #[test]
     fn example_answer_1() {
-        assert_eq!(answer_1(Cursor::new("5 1 9 5\n7 5 3\n2 4 6 8")), 18);
+        assert_eq!(answer_1(&parse_input(Cursor::new("5 1 9 5\n7 5 3\n2 4 6 8"))), 18);
+    }
+
+    #[test]
+    fn example_answer_2() {
+        assert_eq!(answer_2(&parse_input(Cursor::new("5 9 2 8\n9 4 7 3\n3 8 6 5"))), 9);
     }
 }
