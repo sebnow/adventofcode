70d2e31f7e4c31cdc6fc4e873b6cb86c738b64fe
Sebastian Nowicki <sebnow@gmail.com>
2017-12-12 09:10:47 +0100

    2017-12: Add test case for example for part 1

--- 2017/12/src/main.rs
+++ 2017/12/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn parse_input<R: BufRead>(reader: R) -> () {
+    ()
+}
+
+fn answer_1(input: &()) -> i32 {
+    0
+}
+
+fn answer_2(input: &()) -> i32 {
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    let input = parse_input(reader);
+
+    println!("Part 1: {:?}", answer_1(&input));
+    println!("Part 2: {:?}", answer_2(&input));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn example_answer() {
+        let input = Cursor::new(
+            "0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5",
+        );
+        assert_eq!(answer_1(&parse_input(input)), 6);
+    }
 }
