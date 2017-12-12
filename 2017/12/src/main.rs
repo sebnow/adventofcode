252dd8011a1a2db35a903b3533873b316e6ff401
Sebastian Nowicki <sebnow@gmail.com>
2017-12-12 10:56:08 +0100

    2017-12: Solve for part 1

--- 2017/12/src/main.rs
+++ 2017/12/src/main.rs
@@ -1,15 +1,78 @@
+extern crate regex;
+
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use regex::Regex;
 
-fn parse_input<R: BufRead>(reader: R) -> () {
-    ()
+type PID = usize;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Graph(Vec<HashSet<PID>>);
+
+impl Graph {
+    pub fn new() -> Self {
+        Graph(Vec::new())
+    }
+
+    pub fn add_edge(&mut self, a: PID, b: PID) {
+        if self.0.get(a).is_some() {
+            self.0.get_mut(a).unwrap().insert(b);
+        } else {
+            let mut pids = HashSet::new();
+            pids.insert(b);
+            self.0.insert(a, pids);
+        }
+    }
+
+    pub fn count_connected_to(&self, root: PID) -> i32 {
+        let mut seen = HashSet::new();
+        let mut queue = VecDeque::new();
+
+        queue.push_back(root);
+        seen.insert(root);
+
+        while !queue.is_empty() {
+            let p = queue.pop_front().unwrap();
+
+            for &n in self.0.get(p).unwrap() {
+                if !seen.contains(&n) {
+                    seen.insert(n);
+                    queue.push_back(n);
+                }
+            }
+        }
+
+        seen.len() as i32
+    }
 }
 
-fn answer_1(input: &()) -> i32 {
-    0
+fn parse_pids(input: &str) -> Vec<usize> {
+    input.split(", ").map(|x| x.parse().unwrap()).collect()
 }
 
-fn answer_2(input: &()) -> i32 {
+fn parse_input<R: BufRead>(reader: R) -> Graph {
+    let mut g = Graph::new();
+    let re = Regex::new(r"^(\d+) <-> ((?:(?:\d+)(?:, )?)+)$").unwrap();
+
+    for l in reader.lines().map(|l| l.unwrap()) {
+        let caps = re.captures(&l).unwrap();
+        let pid = caps.get(1).unwrap().as_str().parse::<PID>().unwrap();
+
+        for p in caps.get(2).map(|m| parse_pids(m.as_str())).unwrap() {
+            g.add_edge(pid, p);
+        }
+    }
+
+    g
+}
+
+fn answer_1(input: &Graph) -> i32 {
+    input.count_connected_to(0)
+}
+
+fn answer_2(input: &Graph) -> i32 {
     0
 }
 
