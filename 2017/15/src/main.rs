2fd5eb10e06306bcf42115c6f144408d2d84f9f4
Sebastian Nowicki <sebnow@gmail.com>
2017-12-15 16:51:11 +0100

    2017-15: Solve part 1

--- 2017/15/src/main.rs
+++ 2017/15/src/main.rs
@@ -19,15 +19,31 @@ impl Iterator for Generator {
 
     fn next(&mut self) -> Option<Self::Item> {
         let curr = self.curr;
-        println!("{:?} * {:}", curr, self.factor);
-        self.curr = (curr * self.factor) % DENOMINATOR;
+        self.curr = ((curr as u64 * self.factor as u64) % DENOMINATOR as u64) as u32;
 
         Some(self.curr)
     }
 }
 
+#[inline]
+fn judge(a: u32, b: u32) -> bool {
+    let mask: u32 = (2 as u32).pow(16) - 1;
+    (a & mask) == (b & mask)
+}
+
+fn answer_1(a_seed: u32, b_seed: u32) -> usize {
+    let pairs = 40 * 1000 * 1000;
+    let a = Generator::new(16807, a_seed);
+    let b = Generator::new(48271, b_seed);
+
+    a.take(pairs)
+        .zip(b.take(pairs))
+        .filter(|&(a, b)| judge(a, b))
+        .count()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Part 1: {}", answer_1(618, 814));
 }
 
 #[cfg(test)]
@@ -47,5 +63,19 @@ mod test {
             vec![430625591, 1233683848, 1431495498, 137874439, 285222916],
             b.take(5).collect::<Vec<u32>>()
         );
+    }
+
+    #[test]
+    fn test_judge() {
+        assert_eq!(false, judge(245556042, 430625591));
+        assert_eq!(false, judge(1181022009, 1233683848));
+        assert_eq!(true, judge(245556042, 1431495498));
+        assert_eq!(false, judge(1744312007, 137874439));
+        assert_eq!(false, judge(1352636452, 285222916));
+    }
+
+    #[test]
+    fn test_answer_1() {
+        assert_eq!(588, answer_1(65, 8921));
     }
 }
