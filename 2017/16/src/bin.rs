3ec8821a0501de379089f9282344f2cbf2f4c82b
Sebastian Nowicki <sebnow@gmail.com>
2017-12-16 13:43:04 +0100

    2017-16: Add skeleton

---
+++ 2017/16/src/bin.rs
@@ -0,0 +1,6 @@
+extern crate adventofcode201716;
+
+fn main() {
+    println!("Part 1: {}", adventofcode201716::answer_1());
+    println!("Part 2: {}", adventofcode201716::answer_2());
+}
---
+++ 2017/16/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn answer_1() -> String {
+    String::new()
+}
+
+pub fn answer_2() -> String {
+    String::new()
+}
