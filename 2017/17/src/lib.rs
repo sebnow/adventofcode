92800846c99593bf0999fb4d9c045663eebed69f
Sebastian Nowicki <sebnow@gmail.com>
2017-12-17 14:16:01 +0100

    2017-17: Solve both parts

--- 2017/17/src/lib.rs
+++ 2017/17/src/lib.rs
@@ -1,9 +1,38 @@
-pub fn answer_1(input: u32) -> u32 {
-    0
+pub fn answer_1(steps: usize) -> u32 {
+    let mut last_pos = 0;
+    let mut buf: Vec<u32> = Vec::with_capacity(2018);
+    buf.push(0);
+
+    for x in 1..2018 {
+        let pos = wrap_position(buf.len(), last_pos + steps) + 1;
+        buf.insert(pos, x as u32);
+        last_pos = pos;
+    }
+
+    buf[last_pos + 1]
 }
 
-pub fn answer_2(input: u32) -> u32 {
-    0
+pub fn answer_2(steps: usize) -> u32 {
+    let mut last_pos = 0;
+    let mut value = 0;
+
+    for x in 1..50000000 {
+        let pos = wrap_position(x as usize, last_pos + steps) + 1;
+        last_pos = pos;
+        if pos == 1 {
+            value = x
+        }
+    }
+
+    value
+}
+
+fn wrap_position(len: usize, pos: usize) -> usize {
+    match len {
+        0 => 0,
+        1 => 0,
+        _ => pos % len,
+    }
 }
 
 #[cfg(test)]
@@ -11,5 +40,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {}
+    fn example_1() {
+        assert_eq!(638, answer_1(3));
+    }
 }
