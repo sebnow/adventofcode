9688cc0a1e0f8814e7fa25210ac7564f9d76f8a8
Sebastian Nowicki <sebnow@gmail.com>
2017-12-18 19:15:19 +0100

    2017-18: Solve part 1
    Simply changing to a 64bit system probably isn't the best answer for
    this, but oh well...

--- 2017/18/src/instr.rs
+++ 2017/18/src/instr.rs
@@ -2,14 +2,33 @@ use std::str::FromStr;
 use failure::Error;
 
 #[derive(Debug, Eq, PartialEq)]
+pub enum Value {
+    Indirect(char),
+    Direct(i64),
+}
+
+impl FromStr for Value {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.parse::<i64>().map(|x| Value::Direct(x)).or_else(|_| {
+            s.chars()
+                .next()
+                .ok_or(format_err!("expecting integral or char"))
+                .map(|c| Value::Indirect(c))
+        })
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
 pub enum Instr {
-    Set(char, i32),
-    Add(char, i32),
-    Mul(char, char),
-    Mod(char, i32),
+    Set(char, Value),
+    Add(char, Value),
+    Mul(char, Value),
+    Mod(char, Value),
     Snd(char),
     Rcv(char),
-    Jgz(char, i32),
+    Jgz(char, Value),
 }
 
 impl FromStr for Instr {
@@ -22,32 +41,38 @@ impl FromStr for Instr {
         }
 
         let t: &str = instr.get(0).ok_or(format_err!("missing instruction"))?;
-        let get_register = |i: usize| -> Result<char, Self::Err> {
+        let get_register = |t: &str, i: usize| -> Result<char, Self::Err> {
             instr
                 .get(i)
-                .ok_or(format_err!("expecting parameter {} to be a register", i))?
+                .ok_or(format_err!(
+                    "{}: expecting parameter {} to be a register",
+                    t,
+                    i
+                ))?
                 .chars()
                 .next()
-                .ok_or(format_err!("expecting parameter {} to be a character", i))
+                .ok_or(format_err!(
+                    "{}: expecting parameter {} to be a character",
+                    t,
+                    i
+                ))
         };
-        let get_integral = |i: usize| -> Result<i32, Self::Err> {
+        let get_value = |t: &str, i: usize| -> Result<Value, Self::Err> {
             instr
                 .get(i)
-                .ok_or(format_err!("expecting parameter {}", i))?
+                .ok_or(format_err!("{}: expecting parameter {}", t, i))?
                 .parse()
-                .map_err(|e| {
-                    format_err!("expecting parameter {} to be integral: {}", i, e)
-                })
+                .map_err(|e| format_err!("{}: expecting parameter {} to be a value: {}", t, i, e))
         };
 
         match t {
-            "set" => Ok(Instr::Set(get_register(1)?, get_integral(2)?)),
-            "add" => Ok(Instr::Add(get_register(1)?, get_integral(2)?)),
-            "mul" => Ok(Instr::Mul(get_register(1)?, get_register(2)?)),
-            "mod" => Ok(Instr::Mod(get_register(1)?, get_integral(2)?)),
-            "snd" => Ok(Instr::Snd(get_register(1)?)),
-            "rcv" => Ok(Instr::Rcv(get_register(1)?)),
-            "jgz" => Ok(Instr::Jgz(get_register(1)?, get_integral(2)?)),
+            "set" => Ok(Instr::Set(get_register(t, 1)?, get_value(t, 2)?)),
+            "add" => Ok(Instr::Add(get_register(t, 1)?, get_value(t, 2)?)),
+            "mul" => Ok(Instr::Mul(get_register(t, 1)?, get_value(t, 2)?)),
+            "mod" => Ok(Instr::Mod(get_register(t, 1)?, get_value(t, 2)?)),
+            "snd" => Ok(Instr::Snd(get_register(t, 1)?)),
+            "rcv" => Ok(Instr::Rcv(get_register(t, 1)?)),
+            "jgz" => Ok(Instr::Jgz(get_register(t, 1)?, get_value(t, 2)?)),
             _ => Err(format_err!("invalid instruction: {}", t)),
         }
     }
--- 2017/18/src/lib.rs
+++ 2017/18/src/lib.rs
@@ -2,21 +2,74 @@
 extern crate failure;
 
 mod instr;
+mod processor;
 
 use std::str::FromStr;
 
 use instr::*;
+use processor::*;
 
 fn parse_input(input: &str) -> Vec<Instr> {
     input.lines().map(|l| Instr::from_str(l).unwrap()).collect()
 }
 
-pub fn answer_1(input: &str) -> i32 {
-    let _instructions = parse_input(input);
+pub fn answer_1(input: &str) -> i64 {
+    let mut pc = 0;
+    let mut frequency: i64 = 0;
+    let mut processor = Processor::new();
+    let instructions = parse_input(input);
+
+    loop {
+        let mut jmp = 1;
+        let instr = instructions.get(pc).unwrap();
+        let get_value = |p: &Processor, v: &Value| match v {
+            &Value::Direct(x) => x,
+            &Value::Indirect(r) => p.get(r),
+        };
+
+        match instr {
+            &Instr::Snd(r) => frequency = processor.get(r),
+            &Instr::Set(r, ref v) => {
+                let x = get_value(&processor, v);
+                processor.set(r, x);
+            }
+            &Instr::Add(r, ref v) => {
+                let new = processor.get(r) + get_value(&processor, v);
+                processor.set(r, new);
+            }
+            &Instr::Mul(r, ref v) => {
+                let new = processor.get(r) * get_value(&processor, v);
+                processor.set(r, new);
+            }
+            &Instr::Mod(r, ref v) => {
+                let new = processor.get(r) % get_value(&processor, v);
+                processor.set(r, new);
+            }
+            &Instr::Jgz(r, ref offset) => {
+                let v = processor.get(r);
+                if v > 0 {
+                    jmp = get_value(&processor, offset);
+                }
+            }
+            &Instr::Rcv(r) => {
+                let v = processor.get(r);
+                if v > 0 {
+                    return frequency;
+                }
+            }
+        };
+
+        let new_pc = pc as i64 + jmp;
+        if new_pc < 0 || new_pc as usize > instructions.len() {
+            break;
+        }
+        pc = new_pc as usize;
+    }
+
     0
 }
 
-pub fn answer_2(_input: &str) -> i32 {
+pub fn answer_2(_input: &str) -> i64 {
     0
 }
 
@@ -27,16 +80,8 @@ mod test {
     #[test]
     fn example_1() {
         let input = [
-            "set a 1",
-            "add a 2",
-            "mul a a",
-            "mod a 5",
-            "snd a",
-            "set a 0",
-            "rcv a",
-            "jgz a -1",
-            "set a 1",
-            "jgz a -2",
+            "set a 1", "add a 2", "mul a a", "mod a 5", "snd a", "set a 0", "rcv a", "jgz a -1",
+            "set a 1", "jgz a -2",
         ].join("\n");
         assert_eq!(4, answer_1(&input));
     }
