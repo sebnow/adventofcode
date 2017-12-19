71bea9fc6fc5d3617bb7fe5fc4256e0b0e4e89f7
Sebastian Nowicki <sebnow@gmail.com>
2017-12-19 14:03:37 +0100

    2017-19: Solve part 1

--- 2017/19/src/bin.rs
+++ 2017/19/src/bin.rs
@@ -8,6 +8,6 @@ fn main() {
     let mut file = File::open("input.txt").unwrap();
     file.read_to_string(&mut input).unwrap();
 
-    println!("Part 1: {}", adventofcode201719::answer_1(&input));
+    println!("Part 1: {}", adventofcode201719::answer_1(&input).unwrap());
     println!("Part 2: {}", adventofcode201719::answer_2(&input));
 }
