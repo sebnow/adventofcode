ddc7feb1634047a4e8093680ed666872bff18961
Sebastian Nowicki <sebnow@gmail.com>
2019-12-19 10:54:28 +0100

    2019-19: Solve part 1

--- 2019/src/day19.rs
+++ 2019/src/day19.rs
@@ -1,3 +1,4 @@
+use crate::intcode;
 use anyhow::Result;
 
 #[aoc_generator(day19)]
@@ -10,8 +11,27 @@ pub fn input_generator(input: &str) -> Vec<i64> {
 }
 
 #[aoc(day19, part1)]
-fn answer_1(input: &[i64]) -> Result<usize> {
-    Ok(0)
+fn answer_1(input: &[i64]) -> Result<i64> {
+
+    let mut sum = 0;
+    for x in 0..50 {
+        for y in 0..50 {
+            let mut prg = intcode::Interpretor::new(input);
+            prg.input(x);
+            prg.input(y);
+
+            let x = match prg.run()? {
+                intcode::State::Terminated(_) => break,
+                intcode::State::AwaitingInput => panic!("You what? I already gave input"),
+                intcode::State::Suspended(x) => x,
+            };
+
+            print!("{}", x);
+            sum += x;
+        }
+        println!();
+    }
+    Ok(sum)
 }
 
 #[aoc(day19, part2)]
