42b3bd48d32fafc2cb3a88cb54868c58567ddac8
Sebastian Nowicki <sebnow@gmail.com>
2020-12-02 20:00:55 +0100

    2020-01: Use test_example! helper macro

--- 2020/src/bin/day01.rs
+++ 2020/src/bin/day01.rs
@@ -1,42 +1,43 @@
 use anyhow::{anyhow, Result};
 
-fn get_input() -> Result<Vec<u32>> {
-    include_str!("../../input/day01.txt")
-        .lines()
+fn parse_input(s: &str) -> Result<Vec<u32>> {
+    s.lines()
         .map(|l| l.parse().map_err(|e| anyhow!("failed to parse {}", e)))
         .collect()
 }
 
-fn part_one(input: &[u32]) -> Result<String> {
-    for a in input {
-        for b in input {
-            if a+b == 2020 {
-                return Ok(format!("{}", a * b))
+fn part_one(s: &str) -> String {
+    let input = parse_input(s).unwrap();
+    for a in &input {
+        for b in &input {
+            if a + b == 2020 {
+                return format!("{}", a * b);
             }
         }
     }
 
-    Err(anyhow!("answer not found"))
+    "oops".into()
 }
 
-fn part_two(input: &[u32]) -> Result<String>{
-    for a in input {
-        for b in input {
-            for c in input {
-                if a+b+c == 2020 {
-                    return Ok(format!("{}", a * b * c))
+fn part_two(s: &str) -> String {
+    let input = parse_input(s).unwrap();
+    for a in &input {
+        for b in &input {
+            for c in &input {
+                if a + b + c == 2020 {
+                    return format!("{}", a * b * c);
                 }
             }
         }
     }
 
-    Err(anyhow!("answer not found"))
+    "oops".into()
 }
 
 fn main() -> Result<()> {
-    let input = get_input()?;
-    println!("Part one: {}", part_one(&input).unwrap());
-    println!("Part two: {}", part_two(&input).unwrap());
+    let input = include_str!("../../input/day01.txt");
+    println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
 
     Ok(())
 }
@@ -44,15 +45,8 @@ fn main() -> Result<()> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use aocutil::test_example;
 
-    #[test]
-    fn examples_1() {
-        assert_eq!("514579", part_one(&vec![ 1721, 979, 366, 299, 675, 1456 ]).unwrap())
-    }
-
-    #[test]
-    fn examples_2() {
-        assert_eq!("241861950", part_two(&vec![ 1721, 979, 366, 299, 675, 1456 ]).unwrap())
-    }
+    test_example!(example_1_1, part_one, 1, 1, 1);
+    test_example!(example_2_1, part_two, 1, 2, 1);
 }
-
