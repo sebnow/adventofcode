d77090ce7daa4645112080d88c1cbfb92833f206
Sebastian Nowicki <sebnow@gmail.com>
2020-12-05 13:40:27 +0100

    2020-05: Refactor part two to be more functional

--- 2020/src/bin/day05.rs
+++ 2020/src/bin/day05.rs
@@ -2,11 +2,10 @@ fn seat_id(row: u32, column: u32) -> u32 {
     row * 8 + column
 }
 
-fn get_seats<'a> (input: &'a str) -> impl Iterator<Item = u32> + 'a {
-    input
-        .lines()
-        .map(|line| {
-            let res = line.chars().fold(
+fn get_seats<'a>(input: &'a str) -> impl Iterator<Item = u32> + 'a {
+    input.lines().map(|line| {
+        let res =
+            line.chars().fold(
                 (0, 127, 0, 7),
                 |(row_min, row_max, col_min, col_max), c| match c {
                     'F' => (row_min, (row_min + row_max) / 2, col_min, col_max),
@@ -26,28 +25,24 @@ fn get_seats<'a> (input: &'a str) -> impl Iterator<Item = u32> + 'a {
                     _ => panic!("unknown {}", c),
                 },
             );
-            seat_id(res.0, res.2)
-        })
+        seat_id(res.0, res.2)
+    })
 }
 
 fn part_one(input: &str) -> String {
-    get_seats(input)
-        .max()
-        .unwrap()
-        .to_string()
+    get_seats(input).max().unwrap().to_string()
 }
 
 fn part_two(input: &str) -> String {
     let mut seats: Vec<u32> = get_seats(input).collect();
     seats.sort();
 
-    for i in 1 .. seats.len() {
-        if seats[i] + 2 == seats[i+1] {
-            return (seats[i] + 1).to_string()
-        }
-    }
-
-    "".to_string()
+    seats
+        .iter()
+        .zip(seats.iter().skip(1))
+        .find_map(|(&a, &b)| if a + 2 == b { Some(a + 1) } else { None })
+        .unwrap()
+        .to_string()
 }
 
 fn main() {
