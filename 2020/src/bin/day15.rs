b9ebbbd95241e0f9afeb2eddb56dc5f512c7706d
Sebastian Nowicki <sebnow@gmail.com>
2020-12-15 22:47:56 +0100

    2020-15: Refactor

--- 2020/src/bin/day15.rs
+++ 2020/src/bin/day15.rs
@@ -5,7 +5,7 @@ fn parse_input<'a>(s: &'a str) -> impl Iterator<Item = i64> + 'a {
     s.trim().split(',').map(|n| n.parse().unwrap())
 }
 
-fn part_one(input: &str) -> String {
+fn play_game(input: &str, till: i64) -> i64 {
     let mut called: HashMap<i64, i64> = parse_input(input)
         .enumerate()
         .map(|(i, n)| (n, (i + 1) as i64))
@@ -17,34 +17,7 @@ fn part_one(input: &str) -> String {
         .map(|(k, _)| k)
         .unwrap();
 
-    for turn in called.len() as i64 + 1..=2020 {
-        let prevwtf = called.clone(); // TODO Figure out the borrow issue
-        let num = match prevwtf.get(&prev) {
-            Some(t) => turn - 1 - t,
-            None => 0,
-        };
-
-        called.insert(prev, turn - 1);
-
-        prev = num;
-    }
-
-    prev.to_string()
-}
-
-fn part_two(input: &str) -> String {
-    let mut called: HashMap<i64, i64> = parse_input(input)
-        .enumerate()
-        .map(|(i, n)| (n, (i + 1) as i64))
-        .collect();
-
-    let mut prev = *called
-        .iter()
-        .max_by_key(|(_, &v)| v)
-        .map(|(k, _)| k)
-        .unwrap();
-
-    for turn in called.len() as i64 + 1..=30000000 {
+    for turn in called.len() as i64 + 1..=till {
         let num = match called.get(&prev) {
             Some(t) => turn - 1 - t,
             None => 0,
@@ -55,7 +28,15 @@ fn part_two(input: &str) -> String {
         prev = num;
     }
 
-    prev.to_string()
+    prev
+}
+
+fn part_one(input: &str) -> String {
+    play_game(input, 2020).to_string()
+}
+
+fn part_two(input: &str) -> String {
+    play_game(input, 30000000).to_string()
 }
 
 fn main() -> Result<()> {
