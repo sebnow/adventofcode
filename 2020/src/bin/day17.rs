16f684c7c6386a004cfc1101a6b33f2c4736ed3a
Sebastian Nowicki <sebnow@gmail.com>
2020-12-17 17:48:43 +0100

    2020-17: Solve part 2

--- 2020/src/bin/day17.rs
+++ 2020/src/bin/day17.rs
@@ -2,7 +2,40 @@ use euclid;
 use std::collections::HashMap;
 
 type Grid = HashMap<Point3D, Cube>;
+type Grid4D = HashMap<Point4D, Cube>;
 type Point3D = euclid::Point3D<i64, euclid::UnknownUnit>;
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone, Hash)]
+struct Point4D {
+    x: i64,
+    y: i64,
+    z: i64,
+    w: i64,
+}
+
+impl Point4D {
+    pub fn new(x: i64, y: i64, z: i64, w: i64) -> Self {
+        Point4D { x, y, z, w }
+    }
+
+    pub fn min(&self, other: Self) -> Self {
+        Point4D::new(
+            self.x.min(other.x),
+            self.y.min(other.y),
+            self.z.min(other.z),
+            self.w.min(other.w),
+        )
+    }
+
+    pub fn max(&self, other: Self) -> Self {
+        Point4D::new(
+            self.x.max(other.x),
+            self.y.max(other.y),
+            self.z.max(other.z),
+            self.w.max(other.w),
+        )
+    }
+}
 
 #[derive(PartialEq, Debug, Copy, Clone)]
 enum Cube {
@@ -19,11 +52,25 @@ impl Cube {
     }
 }
 
-fn neighbours(g: &Grid, p: &Point3D) -> Vec<(Point3D, Cube)> {
+fn neighbours3(g: &Grid, p: &Point3D) -> Vec<(Point3D, Cube)> {
     (-1..=1)
         .flat_map(move |z| {
             (-1..=1)
                 .flat_map(move |y| (-1..=1).map(move |x| Point3D::new(p.x + x, p.y + y, p.z + z)))
+        })
+        .filter(|n| n != p)
+        .map(move |n| (n, g.get(&n).map(move |c| *c).unwrap_or(Cube::Inactive)))
+        .collect()
+}
+
+fn neighbours4(g: &Grid4D, p: &Point4D) -> Vec<(Point4D, Cube)> {
+    (-1..=1)
+        .flat_map(move |w| {
+            (-1..=1).flat_map(move |z| {
+                (-1..=1).flat_map(move |y| {
+                    (-1..=1).map(move |x| Point4D::new(p.x + x, p.y + y, p.z + z, p.w + w))
+                })
+            })
         })
         .filter(|n| n != p)
         .map(move |n| (n, g.get(&n).map(move |c| *c).unwrap_or(Cube::Inactive)))
@@ -40,7 +87,17 @@ fn get_bounds(g: &Grid) -> (Point3D, Point3D) {
     )
 }
 
-fn run_cycle(grid: &mut Grid) {
+fn get_bounds4(g: &Grid4D) -> (Point4D, Point4D) {
+    g.iter().filter(|(_, c)| c.is_active()).fold(
+        (
+            Point4D::new(std::i64::MAX, std::i64::MAX, std::i64::MAX, std::i64::MAX),
+            Point4D::new(std::i64::MIN, std::i64::MIN, std::i64::MIN, std::i64::MIN),
+        ),
+        |(l, u), (p, _)| (l.min(*p), u.max(*p)),
+    )
+}
+
+fn run_cycle3(grid: &mut Grid) {
     let prev = grid.clone();
 
     let (lower_bound, upper_bound) = get_bounds(&prev);
@@ -49,7 +106,7 @@ fn run_cycle(grid: &mut Grid) {
             for x in (lower_bound.x - 1)..=(upper_bound.x + 1) {
                 let p = Point3D::new(x, y, z);
                 let c = prev.get(&p).unwrap_or(&Cube::Inactive);
-                let ns = neighbours(&prev, &p);
+                let ns = neighbours3(&prev, &p);
                 let nc = ns.iter().filter(|(_, nc)| nc.is_active()).count();
 
                 let new = match c {
@@ -75,6 +132,43 @@ fn run_cycle(grid: &mut Grid) {
     }
 }
 
+fn run_cycle4(grid: &mut Grid4D) {
+    let prev = grid.clone();
+
+    let (lower_bound, upper_bound) = get_bounds4(&prev);
+    for w in (lower_bound.w - 1)..=(upper_bound.w + 1) {
+        for z in (lower_bound.z - 1)..=(upper_bound.z + 1) {
+            for y in (lower_bound.y - 1)..=(upper_bound.y + 1) {
+                for x in (lower_bound.x - 1)..=(upper_bound.x + 1) {
+                    let p = Point4D::new(x, y, z, w);
+                    let c = prev.get(&p).unwrap_or(&Cube::Inactive);
+                    let ns = neighbours4(&prev, &p);
+                    let nc = ns.iter().filter(|(_, nc)| nc.is_active()).count();
+
+                    let new = match c {
+                        Cube::Active => {
+                            if nc == 2 || nc == 3 {
+                                Cube::Active
+                            } else {
+                                Cube::Inactive
+                            }
+                        }
+                        Cube::Inactive => {
+                            if nc == 3 {
+                                Cube::Active
+                            } else {
+                                Cube::Inactive
+                            }
+                        }
+                    };
+
+                    grid.insert(p, new);
+                }
+            }
+        }
+    }
+}
+
 fn parse_input<'a>(s: &'a str) -> impl Iterator<Item = (Point3D, Cube)> + 'a {
     s.lines().enumerate().flat_map(move |(y, l)| {
         l.chars().enumerate().map(move |(x, c)| {
@@ -94,7 +188,7 @@ fn part_one(input: &str) -> String {
     let mut grid: Grid = parse_input(input).collect();
 
     for _ in 0..6 {
-        run_cycle(&mut grid);
+        run_cycle3(&mut grid);
     }
 
     grid.iter()
@@ -103,8 +197,19 @@ fn part_one(input: &str) -> String {
         .to_string()
 }
 
-fn part_two(_input: &str) -> String {
-    "".to_string()
+fn part_two(input: &str) -> String {
+    let mut grid: Grid4D = parse_input(input)
+        .map(|(p, c)| (Point4D::new(p.x, p.y, p.z, 0), c))
+        .collect();
+
+    for _ in 0..6 {
+        run_cycle4(&mut grid);
+    }
+
+    grid.iter()
+        .filter(|(_, c)| c.is_active())
+        .count()
+        .to_string()
 }
 
 fn main() {
@@ -119,5 +224,5 @@ mod test {
     use aocutil::test_example;
 
     test_example!(example_one_1, part_one, 17, 1, 1);
-    //test_example!(example_two_1, part_two, 17, 2, 1);
+    test_example!(example_two_1, part_two, 17, 2, 1);
 }
