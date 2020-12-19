251844516c1383d086967da1a5ac39358e6d1546
Sebastian Nowicki <sebnow@gmail.com>
2020-12-19 13:48:23 +0100

    2020-19: Solve part 2

--- 2020/src/bin/day19.rs
+++ 2020/src/bin/day19.rs
@@ -1,134 +1,127 @@
-use std::collections::{HashMap, VecDeque};
-
-#[derive(Copy, Clone)]
-struct Cursor<'a> {
-    s: &'a [u8],
-    idx: usize,
-}
-
-impl<'a> Cursor<'a> {
-    fn new(s: &'a [u8], idx: usize) -> Self {
-        Self { s, idx }
-    }
-
-    fn peek(&self) -> u8 {
-        self.s[self.idx]
-    }
-
-    fn next(&mut self) -> u8 {
-        let c = self.peek();
-        self.idx += 1;
-        c
-    }
-}
+use anyhow::{anyhow, Result};
+use std::collections::HashMap;
 
 #[derive(Clone, PartialEq, Debug)]
 enum Rule {
-    Char(u8),
-    Seq(Vec<Box<Rule>>),
-    Alt(Box<Rule>, Box<Rule>),
+    Char(char),
+    Seq(Vec<i64>),
+    Alt(Vec<i64>, Vec<i64>),
 }
 
-impl Rule {
-    pub fn matches(&self, s: &[u8]) -> bool {
-        let mut cur = Cursor::new(s, 0);
-        self.matches_st(&mut cur) && cur.idx == cur.s.len()
-    }
+impl std::str::FromStr for Rule {
+    type Err = anyhow::Error;
 
-    fn matches_st(&self, mut cur: &mut Cursor) -> bool {
-        let m = match self {
-            Rule::Char(r) => cur.next() == *r,
-            Rule::Seq(rs) => rs.iter().all(|r| r.matches_st(&mut cur)),
-            Rule::Alt(a, b) => {
-                let mut sub_cur = cur.clone();
-                if a.matches_st(&mut sub_cur) {
-                    *cur = sub_cur;
-                    true
-                } else {
-                    let mut sub_cur = cur.clone();
-                    if b.matches_st(&mut sub_cur) {
-                        *cur = sub_cur;
-                        true
-                    } else {
-                        false
-                    }
-                }
-            }
-        };
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.contains('|') {
+            let mut parts = s.split(" | ");
+            let left = parts
+                .next()
+                .ok_or_else(|| anyhow!("missing left alternate"))?
+                .split(' ')
+                .map(|n| n.parse().unwrap())
+                .collect();
+            let right = parts
+                .next()
+                .ok_or_else(|| anyhow!("missing right alternate"))?
+                .split(' ')
+                .map(|n| n.parse().unwrap())
+                .collect();
 
-        m
-    }
-}
+            Ok(Rule::Alt(left, right))
+        } else if s.starts_with('"') {
+            let c = s
+                .chars()
+                .nth(1)
+                .ok_or_else(|| anyhow!("missing reference"))?;
 
-impl std::fmt::Display for Rule {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use Rule::*;
-        match self {
-            Char(c) => write!(f, "{}", *c as char),
-            Alt(a, b) => write!(f, "({}|{})", a, b),
-            Seq(rs) => {
-                for r in rs {
-                    write!(f, "{}", r)?;
-                }
-                Ok(())
-            }
+            Ok(Rule::Char(c))
+        } else {
+            let ids = s
+                .split(' ')
+                .map(|id| {
+                    id.parse()
+                        .map_err(|err| anyhow!("invalid reference: {}", err))
+                })
+                .collect::<Result<_>>()?;
+
+            Ok(Rule::Seq(ids))
         }
     }
 }
 
-fn maybe_alt(l: Option<Rule>, r: Option<Rule>) -> Option<Rule> {
-    Some(Rule::Alt(Box::new(l?), Box::new(r?)))
+struct RuleEngine {
+    rules: HashMap<i64, Rule>,
 }
 
-fn parse_rule(s: &str, m: &HashMap<i64, Rule>) -> Option<Rule> {
-    if s.contains('|') {
-        let mut parts = s.split(" | ");
-        let left = parts.next().map(|l| parse_rule(l, m)).flatten();
-        let right = parts.next().map(|r| parse_rule(r, m)).flatten();
-        return maybe_alt(left, right);
+impl RuleEngine {
+    pub fn new(rules: HashMap<i64, Rule>) -> Self {
+        RuleEngine { rules }
     }
 
-    let rules: Option<Vec<Rule>> = s
-        .split(" ")
-        .map(|part| match part.chars().nth(0).unwrap() {
-            '"' => Some(Rule::Char(part.bytes().nth(1).expect("missing char"))),
-            _ => m
-                .get(&part.parse().expect(&format!("invalid id '{}'", part)))
-                .map(|r| r.clone()),
+    pub fn matches(&self, s: &str) -> bool {
+        self.match_rule_id(s, 0).contains(&Some(""))
+    }
+
+    fn match_rule_id<'a>(&self, s: &'a str, rule_id: i64) -> Vec<Option<&'a str>> {
+        let rule = self.rules.get(&rule_id).unwrap();
+
+        self.match_rule(s, rule)
+    }
+
+    fn match_rule<'a>(&self, s: &'a str, rule: &Rule) -> Vec<Option<&'a str>> {
+        match rule {
+            Rule::Char(c) if s.chars().next() == Some(*c) => vec![Some(&s[1..])],
+            Rule::Char(_) => vec![None],
+            Rule::Seq(rs) => self.match_seq(s, rs),
+            Rule::Alt(left, right) => self.match_alt(s, left, right),
+        }
+    }
+
+    fn match_seq<'a>(&self, s: &'a str, rules: &[i64]) -> Vec<Option<&'a str>> {
+        rules.iter().fold(vec![Some(s)], |ss, r| {
+            ss.iter()
+                .flat_map(|s| match s {
+                    Some(s) if !s.is_empty() => self.match_rule_id(s, *r),
+                    _ => vec![None],
+                })
+                .collect()
         })
-        .collect();
-
-    rules.map(|rs| {
-        if rs.len() == 1 {
-            return rs[0].clone();
-        } else {
-            return Rule::Seq(rs.iter().map(|r| Box::new(r.clone())).collect());
-        }
-    })
-}
-
-fn parse_input<'a>(input: &'a str) -> (Rule, impl Iterator<Item = &'a str> + 'a) {
-    let mut parts = input.split("\n\n");
-    let mut rule_map: HashMap<i64, Rule> = HashMap::new();
-    let mut rule_queue: VecDeque<&str> = parts.next().unwrap().lines().collect();
-
-    while let Some(s) = rule_queue.pop_front() {
-        let mut rule_parts = s.split(": ");
-        let rule_id = rule_parts
-            .next()
-            .unwrap()
-            .parse()
-            .expect("rule id is not a number");
-
-        if let Some(r) = parse_rule(rule_parts.next().unwrap(), &rule_map) {
-            rule_map.insert(rule_id, r);
-        } else {
-            rule_queue.push_back(s);
-        }
     }
 
+    fn match_alt<'a>(&self, s: &'a str, left: &[i64], right: &[i64]) -> Vec<Option<&'a str>> {
+        [left, right]
+            .iter()
+            .flat_map(|rs| self.match_seq(s, rs))
+            .collect()
+    }
+}
+
+impl std::str::FromStr for RuleEngine {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let rules = s
+            .lines()
+            .map(|l| {
+                let mut parts = l.split(": ");
+                Ok((
+                    parts.next().ok_or_else(|| anyhow!("missing id"))?.parse()?,
+                    parts
+                        .next()
+                        .ok_or_else(|| anyhow!("missing definition"))?
+                        .parse()?,
+                ))
+            })
+            .collect::<Result<_>>()?;
+
+        Ok(RuleEngine::new(rules))
+    }
+}
+
+fn parse_input<'a>(input: &'a str) -> (RuleEngine, impl Iterator<Item = &'a str> + 'a) {
+    let mut parts = input.split("\n\n");
     (
-        rule_map.get(&0).unwrap().to_owned(),
+        parts.next().unwrap().parse().unwrap(),
         parts.next().unwrap().lines(),
     )
 }
@@ -136,14 +129,25 @@ fn parse_input<'a>(input: &'a str) -> (Rule, impl Iterator<Item = &'a str> + 'a)
 fn part_one(input: &str) -> String {
     let (rules, messages) = parse_input(input);
 
-    messages
-        .filter(|m| rules.matches(m.as_bytes()))
-        .count()
-        .to_string()
+    messages.filter(|m| rules.matches(m)).count().to_string()
 }
 
 fn part_two(input: &str) -> String {
-    "".to_string()
+    let input: String = input
+        .lines()
+        .map(|l| {
+            if l.starts_with("8: ") {
+                "8: 42 | 42 8"
+            } else if l.starts_with("11: ") {
+                "11: 42 31 | 42 11 31"
+            } else {
+                l
+            }
+        })
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    part_one(&input)
 }
 
 fn main() {
@@ -156,6 +160,7 @@ fn main() {
 mod test {
     use super::*;
     use aocutil::test_example;
+    use std::str::FromStr;
 
     test_example!(example_one_1, part_one, 19, 1, 1);
     test_example!(example_one_2, part_one, 19, 1, 2);
@@ -163,68 +168,76 @@ mod test {
     test_example!(example_one_4, part_one, 19, 1, 4);
     test_example!(example_one_5, part_one, 19, 1, 5);
     test_example!(example_one_6, part_one, 19, 1, 6);
-    //test_example!(example_two_2, part_two, 19, 2, 1);
+    test_example!(example_two_1, part_two, 19, 2, 1);
+    test_example!(example_two_2, part_two, 19, 2, 2);
+    test_example!(example_two_3, part_two, 19, 2, 3);
+    test_example!(example_two_4, part_two, 19, 2, 4);
+    test_example!(example_two_5, part_two, 19, 2, 5);
 
     #[test]
-    fn parse_char() {
-        assert_eq!(parse_rule("\"a\"", &HashMap::new()), Some(Rule::Char(b'a')));
+    fn parse_char() -> Result<()> {
+        assert_eq!(Rule::from_str("\"a\"")?, Rule::Char('a'));
+        Ok(())
     }
 
     #[test]
-    fn parse_seq() {
-        let a = Rule::Char(b'a');
-        let b = Rule::Char(b'b');
-        let mut m = HashMap::new();
-        m.insert(1, a.clone());
-        m.insert(2, b.clone());
-
-        assert_eq!(
-            parse_rule("1 2", &m),
-            Some(Rule::Seq(vec![Box::new(a), Box::new(b)]))
-        );
+    fn parse_seq() -> Result<()> {
+        assert_eq!(Rule::from_str("1 2")?, Rule::Seq(vec![1, 2]));
+        Ok(())
     }
 
     #[test]
-    fn parse_alt() {
-        let a = Rule::Char(b'a');
-        let b = Rule::Char(b'b');
-        let mut m = HashMap::new();
-        m.insert(1, a.clone());
-        m.insert(2, b.clone());
-
-        assert_eq!(
-            parse_rule("1 | 2", &m),
-            Some(Rule::Alt(Box::new(a), Box::new(b)))
-        );
+    fn parse_alt() -> Result<()> {
+        assert_eq!(Rule::from_str("1 | 2")?, alt(vec![1], vec![2]),);
+        Ok(())
     }
 
     #[test]
     fn match_char() {
-        assert!(Rule::Char(b'a').matches(&[b'a']));
+        let mut rules = HashMap::new();
+        rules.insert(0, Rule::Char('a'));
+
+        assert!(RuleEngine::new(rules).matches("a"));
     }
 
     #[test]
     fn match_seq() {
-        assert!(
-            Rule::Seq(vec![Box::new(Rule::Char(b'a')), Box::new(Rule::Char(b'b'))])
-                .matches("ab".as_bytes())
-        );
+        let mut rules = HashMap::new();
+        rules.insert(0, Rule::Seq(vec![1, 2]));
+        rules.insert(1, Rule::Char('a'));
+        rules.insert(2, Rule::Char('b'));
+
+        assert!(RuleEngine::new(rules).matches("ab"));
     }
 
     #[test]
     fn match_alt() {
-        let rule = Rule::Alt(Box::new(Rule::Char(b'a')), Box::new(Rule::Char(b'b')));
-        assert!(rule.matches("a".as_bytes()));
-        assert!(rule.matches("b".as_bytes()));
+        let mut rules = HashMap::new();
+        rules.insert(0, alt(vec![1], vec![2]));
+        rules.insert(1, Rule::Char('a'));
+        rules.insert(2, Rule::Char('b'));
+
+        let engine = RuleEngine::new(rules);
+        assert!(engine.matches("a"));
+        assert!(engine.matches("b"));
     }
 
     #[test]
     fn match_seq_alt() {
-        let alt = Rule::Alt(Box::new(Rule::Char(b'a')), Box::new(Rule::Char(b'b')));
-        let rule = Rule::Seq(vec![Box::new(alt.clone()), Box::new(alt)]);
-        assert!(rule.matches("aa".as_bytes()));
-        assert!(rule.matches("ab".as_bytes()));
-        assert!(rule.matches("ba".as_bytes()));
-        assert!(rule.matches("bb".as_bytes()));
+        let mut rules = HashMap::new();
+        rules.insert(0, Rule::Seq(vec![1, 1]));
+        rules.insert(1, alt(vec![2], vec![3]));
+        rules.insert(2, Rule::Char('a'));
+        rules.insert(3, Rule::Char('b'));
+
+        let engine = RuleEngine::new(rules);
+        assert!(engine.matches("aa"));
+        assert!(engine.matches("ab"));
+        assert!(engine.matches("ba"));
+        assert!(engine.matches("bb"));
+    }
+
+    fn alt(left: Vec<i64>, right: Vec<i64>) -> Rule {
+        Rule::Alt(left, right)
     }
 }
