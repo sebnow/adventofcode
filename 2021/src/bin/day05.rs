ed61edddc19364e13feebc286ce7d65475cd3ffb
Sebastian Nowicki <sebnow@gmail.com>
2021-12-05 12:00:35 +0100

    2021-05: Refactor line generation
    Surely there's a better way to do this. Not being able to use the range
    operator with a negative step is quite annoying.

--- 2021/src/bin/day05.rs
+++ 2021/src/bin/day05.rs
@@ -4,51 +4,44 @@ use std::iter::Iterator;
 
 type Segment = (Point, Point);
 
-fn parse_input<'a>(s: &'a str) -> impl Iterator<Item = Segment> + 'a{
-    s.lines()
-        .map(|l| {
-            let mut points = l.split(" -> ").map(|p| {
-                let mut ns = p.split(",").map(|n| n.parse().unwrap());
+fn parse_input<'a>(s: &'a str) -> impl Iterator<Item = Segment> + 'a {
+    s.lines().map(|l| {
+        let mut points = l.split(" -> ").map(|p| {
+            let mut ns = p.split(",").map(|n| n.parse().unwrap());
 
-                Point::new(ns.next().unwrap(), ns.next().unwrap())
-            });
+            Point::new(ns.next().unwrap(), ns.next().unwrap())
+        });
 
-            (points.next().unwrap(), points.next().unwrap())
-        })
+        (points.next().unwrap(), points.next().unwrap())
+    })
 }
 
 fn points((a, b): Segment) -> Vec<Point> {
-    let xs: Vec<i64> = if a.x < b.x {
-        (a.x..=b.x).collect()
-    } else {
-        ((b.x..=a.x).rev()).collect()
-    };
-    let ys: Vec<i64> = if a.y < b.y {
-        (a.y..=b.y).collect()
-    } else {
-        ((b.y..=a.y).rev()).collect()
-    };
+    let mut xs: Vec<i64> = (a.x.min(b.x)..=a.x.max(b.x)).collect();
+    if a.x > b.x {
+        xs.reverse();
+    }
 
-    if a.x == b.x {
+    let mut ys: Vec<i64> = (a.y.min(b.y)..=a.y.max(b.y)).collect();
+    if a.y > b.y {
+        ys.reverse();
+    }
+
+    if xs.len() == 1 {
         xs.iter()
             .cycle()
             .zip(ys.iter())
             .map(|(&x, &y)| Point::new(x, y))
             .collect()
-    } else if a.y == b.y {
-        xs.iter()
-            .zip(ys.iter().cycle())
-            .map(|(&x, &y)| Point::new(x, y))
-            .collect()
     } else {
         xs.iter()
-            .zip(ys.iter())
+            .zip(ys.iter().cycle())
             .map(|(&x, &y)| Point::new(x, y))
             .collect()
     }
 }
 
-fn count_crossing<I: Iterator<Item=Segment>>(segments: I) -> usize {
+fn count_crossing<I: Iterator<Item = Segment>>(segments: I) -> usize {
     let mut grid: HashMap<Point, usize> = HashMap::new();
 
     for s in segments {
@@ -62,7 +55,10 @@ fn count_crossing<I: Iterator<Item=Segment>>(segments: I) -> usize {
 
 fn part_one(s: &str) -> String {
     let segments = parse_input(s);
-    format!("{}", count_crossing(segments.filter(|(a, b)| a.x == b.x || a.y == b.y)))
+    format!(
+        "{}",
+        count_crossing(segments.filter(|(a, b)| a.x == b.x || a.y == b.y))
+    )
 }
 
 fn part_two(s: &str) -> String {
@@ -83,4 +79,17 @@ mod test {
 
     test_example!(example_5_1, part_one, 5, 1, 1);
     test_example!(example_5_2, part_two, 5, 2, 1);
+
+    #[test]
+    fn diagonal() {
+        assert_eq!(
+            points((Point::new(0, 2), Point::new(2, 0))),
+            vec![Point::new(0, 2), Point::new(1, 1), Point::new(2, 0)]
+        );
+
+        assert_eq!(
+            points((Point::new(0, 0), Point::new(2, 2))),
+            vec![Point::new(0, 0), Point::new(1, 1), Point::new(2, 2)]
+        );
+    }
 }
