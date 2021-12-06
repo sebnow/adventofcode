29c21e4616c16ba3e6be66b4192cbd30b69e722c
Sebastian Nowicki <sebnow@gmail.com>
2021-12-06 17:14:01 +0100

    2021-06: Refactor to common solution

--- 2021/src/bin/day06.rs
+++ 2021/src/bin/day06.rs
@@ -4,38 +4,19 @@ struct Fish {
 }
 
 fn parse_input<'a>(s: &'a str) -> impl Iterator<Item = Fish> + 'a {
-    s.trim().split(",").map(|x| {
-        Fish {
-            days_to_spawn: x.parse().unwrap(),
-        }
+    s.trim().split(",").map(|x| Fish {
+        days_to_spawn: x.parse().unwrap(),
     })
 }
 
-fn part_one(s: &str) -> String {
-    let mut fishies: Vec<_> = parse_input(s).collect();
-
-    for _ in 0..80 {
-        for i in 0..fishies.len() {
-            if fishies[i].days_to_spawn == 0 {
-                fishies.push(Fish { days_to_spawn: 8 });
-                fishies[i].days_to_spawn = 7;
-            }
-
-            fishies[i].days_to_spawn -= 1;
-        }
-    }
-
-    format!("{}", fishies.len())
-}
-
-fn part_two(s: &str) -> String {
+fn breed_for_days(s: &str, n: usize) -> usize {
     let mut fishies = [0 as usize; 9];
 
     for f in parse_input(s) {
         fishies[f.days_to_spawn as usize] += 1;
     }
 
-    for _ in 0..256 {
+    for _ in 0..n {
         let baby_fishies = fishies[0];
         for i in 1..fishies.len() {
             fishies[i - 1] = fishies[i];
@@ -45,7 +26,15 @@ fn part_two(s: &str) -> String {
         fishies[8] = baby_fishies;
     }
 
-    format!("{}", fishies.iter().sum::<usize>())
+    fishies.iter().sum()
+}
+
+fn part_one(s: &str) -> String {
+    format!("{}", breed_for_days(s, 80))
+}
+
+fn part_two(s: &str) -> String {
+    format!("{}", breed_for_days(s, 256))
 }
 
 fn main() {
