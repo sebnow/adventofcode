c1f6c84ff47590a4e7998b095d4aadcae13b93fd
Sebastian Nowicki <sebnow@gmail.com>
2021-12-08 23:14:16 +0100

    2021-08: Clean up part 2
    Idea of matching on the amount of intersections stolen from
    @jamescosford.

--- 2021/src/bin/day08.rs
+++ 2021/src/bin/day08.rs
@@ -1,61 +1,12 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-// Amount of segments -> Possible Digits
-// 1 -> []
-// 2 -> [1]
-// 3 -> [7]
-// 4 -> [4]
-// 5 -> [2, 3, 5]
-// 6 -> [0, 6, 9]
-// 7 -> 8
-
-// Digit -> Segments
-// 0 -> [A, B, C, E, F, G]
-// 1 -> [C, F]
-// 2 -> [A, C, D, E, F]
-// 3 -> [A, C, D, F, G]
-// 4 -> [B, C, D, F]
-// 5 -> [A, B, D, F, G]
-// 6 -> [A, B, D, E, F, G]
-// 7 -> [A, C, F]
-// 8 -> [A, B, C, D, E, F, G]
-// 9 -> [A, B, C, D, F, G]
-
-type Signal = Vec<char>;
+type Signal = HashSet<char>;
 
 #[derive(Debug)]
 struct Entry {
     signals: Vec<Signal>,
     output: Vec<Signal>,
 }
-
-#[derive(Debug, Copy, Clone)]
-enum Segment {
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-}
-
-//fn segments_to_digit(s: &[Segment]) -> u32 {
-//    use Segment::*;
-//
-//    match s {
-//        [A, B, C, E, F, G] => 0,
-//        [C, F] => 1,
-//        [A, C, D, E, F] => 3,
-//        [B, C, D, F] => 4,
-//        [A, B, D, F, G] => 5,
-//        [A, B, D, E, F, G] => 6,
-//        [A, C, F] => 7,
-//        [A, B, C, D, E, F, G] => 8,
-//        [A, B, C, D, F, G] => 9,
-//        _ => panic!("invalid segments: {:?}", s),
-//    }
-//}
 
 fn parse_input(s: &str) -> impl Iterator<Item = Entry> + '_ {
     s.lines().map(|s| {
@@ -77,169 +28,66 @@ fn part_one(s: &str) -> String {
         entries
             .iter()
             .flat_map(|e| {
-                e.output.iter().filter(|&signals| match signals.len() {
-                    2 | 3 | 4 | 7 => true,
-                    _ => false,
-                })
+                e.output
+                    .iter()
+                    .filter(|&signals| matches!(signals.len(), 2 | 3 | 4 | 7))
             })
             .count()
     )
 }
 
-// TODO: This is ridiculous. Find an algorithm, dummy.
-fn detect_wires(entry: &Entry) -> Vec<(HashSet<char>, u32)> {
-    let signals: Vec<HashSet<char>> = entry
-        .signals
-        .iter()
-        .map(|v| v.iter().copied().collect())
-        .collect();
-    let mut digits: HashMap<u32, HashSet<char>> = HashMap::with_capacity(9);
-    //let mut wires: HashMap<char, Segment> = HashMap::with_capacity(9);
-
-    let signal_of_len = |l: usize| {
-        signals
-            .iter()
-            .find(|s| s.len() == l)
-            .expect(&format!("no unique signal of length {}", l))
-            .clone()
-    };
-
-    digits.insert(1, signal_of_len(2));
-    digits.insert(7, signal_of_len(3));
-    digits.insert(4, signal_of_len(4));
-    digits.insert(8, signal_of_len(7));
-
-    // 3 = len == 5 && 1
-    {
-        let one = digits.get(&1).unwrap();
-        let s3 = signals
-            .iter()
-            .find(|s| s.len() == 5 && one.is_subset(s))
-            .unwrap()
-            .clone();
-
-        println!("3: {:?}", s3);
-        digits.insert(3, s3);
+fn map_signal(
+    s: &HashSet<char>,
+    d1: &HashSet<char>,
+    d4: &HashSet<char>,
+    d7: &HashSet<char>,
+) -> u32 {
+    // Idea stolen from @jamescosford. So nice.
+    match (
+        s.len(),
+        s.intersection(d1).count(),
+        s.intersection(d4).count(),
+        s.intersection(d7).count(),
+    ) {
+        (2, _, _, _) => 1,
+        (5, 1, 2, 2) => 2,
+        (5, 2, 3, 3) => 3,
+        (4, _, _, _) => 4,
+        (5, 1, 3, 2) => 5,
+        (6, 1, 3, 2) => 6,
+        (3, _, _, _) => 7,
+        (7, _, _, _) => 8,
+        (6, 2, 4, 3) => 9,
+        (6, 2, 3, 3) => 0,
+        _ => panic!("invalid signal"),
     }
-
-    // 9 = len == 6 && 4
-    {
-        let four = digits.get(&4).unwrap();
-        let s9 = signals
-            .iter()
-            .find(|s| s.len() == 6 && four.is_subset(s))
-            .unwrap()
-            .clone();
-
-        println!("9: {:?}", s9);
-        digits.insert(9, s9);
-    }
-
-    // 6 = len == 6 && !1
-    {
-        let one = digits.get(&1).unwrap();
-        let s6 = signals
-            .iter()
-            .find(|s| s.len() == 6 && !one.is_subset(s))
-            .unwrap()
-            .clone();
-
-        // 5 = 6 - E
-        //let s5 = &s6 - &e_unique_segments;
-        //digits.insert(5, s5);
-
-        println!("6: {:?}", s6);
-        digits.insert(6, s6);
-    }
-
-    // 5 = len == 5 && 6
-    {
-        let six = digits.get(&6).unwrap();
-        let s5 = signals
-            .iter()
-            .find(|s| s.len() == 5 && six.is_superset(s))
-            .unwrap()
-            .clone();
-
-        println!("--> 6: {:?}", six);
-        println!("5: {:?}", s5);
-        digits.insert(5, s5);
-    }
-
-    // 2 = len == 5 && !3 && !5
-    {
-        let three = digits.get(&3).unwrap();
-        let five = digits.get(&5).unwrap();
-        let s2 = signals
-            .iter()
-            .find(|s| s.len() == 5 && s != &three && s != &five)
-            .unwrap()
-            .clone();
-
-        println!("2: {:?}", s2);
-        digits.insert(2, s2);
-    }
-
-    // 7 - 1 = A
-    //let a_unique_segments = digits.get(&7).unwrap() - digits.get(&1).unwrap();
-    //let seg_a = *a_unique_segments.iter().next().unwrap();
-    //wires.insert(seg_a, Segment::A);
-
-    // G = 9 - (4 + a)
-    //    let mut a4 = digits.get(&4).unwrap().clone();
-    //    a4.insert(seg_a);
-    //    for s in &signals {
-    //        let diff = s - &a4;
-    //        if a4.is_subset(s) && diff.len() == 1 {
-    //            let seg_g = *diff.iter().next().unwrap();
-    //            wires.insert(seg_g, Segment::G);
-    //            digits.insert(9, s.clone());
-    //            break;
-    //        }
-    //    }
-
-    // E = 9 - 8
-    //let e_unique_segments = digits.get(&8).unwrap() - digits.get(&9).unwrap();
-    //let seg_e = *e_unique_segments.iter().next().unwrap();
-    //wires.insert(seg_e, Segment::E);
-
-    // 0 = len == 6 && 1 && !3
-    // 0 = len == 6 && !5
-    {
-        let five = digits.get(&5).unwrap();
-        let s0 = signals
-            .iter()
-            .find(|s| s.len() == 6 && !s.is_superset(five))
-            .unwrap()
-            .clone();
-        println!("0: {:?}", s0);
-        digits.insert(0, s0);
-    }
-
-    // B = 9 - 3
-    //let b_unique_segments = digits.get(&9).unwrap() - digits.get(&3).unwrap();
-    //wires.insert(b_unique_segments.iter().next().unwrap().clone(), Segment::B);
-
-    println!("Digits[{}/10]: {:?}", digits.len(), digits);
-    //println!("Wires [{}/10]: {:?}", wires.len(), wires);
-    digits.iter().map(|(k, v)| (v.clone(), *k)).collect()
 }
 
-fn get_output(entry: &Entry) -> u32 {
-    let wires = detect_wires(entry);
+fn map_output(entry: &Entry) -> u32 {
+    let signal_of_len = |l: usize| {
+        entry
+            .signals
+            .iter()
+            .find(|s| s.len() == l)
+            .unwrap_or_else(|| panic!("no unique signal of length {}", l))
+    };
 
-    entry.output.iter().fold(0, |x: u32, signal: &Signal| {
-        let set: HashSet<char> = signal.iter().copied().collect();
-        //println!("looking for {:?} ({:?}) in {:?}", signal, set, wires);
-        let digit = wires.iter().find(|(s, _)| s == &set).unwrap().1;
-        x * 10 + digit
-    })
+    let d1 = signal_of_len(2);
+    let d4 = signal_of_len(4);
+    let d7 = signal_of_len(3);
+
+    entry
+        .output
+        .iter()
+        .fold(0, |x: u32, signal: &HashSet<char>| {
+            x * 10 + map_signal(signal, d1, d4, d7)
+        })
 }
 
 fn part_two(s: &str) -> String {
     let entries: Vec<_> = parse_input(s).collect();
 
-    format!("{}", entries.iter().map(get_output).sum::<u32>())
+    format!("{}", entries.iter().map(map_output).sum::<u32>())
 }
 
 fn main() {
