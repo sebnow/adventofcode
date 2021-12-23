9193b7f9181cd2249fd889c8e6723054f4e5b789
Sebastian Nowicki <sebnow@gmail.com>
2021-12-23 21:42:46 +0100

    2021-21: Solve part 2

--- 2021/src/bin/day21.rs
+++ 2021/src/bin/day21.rs
@@ -1,32 +1,27 @@
 use itertools::Itertools;
+use lazy_static::lazy_static;
+use memoize::memoize;
+use std::collections::HashMap;
 
 const BOARD_SIZE: u64 = 10;
-const WINNING_SCORE: u64 = 1_000;
 
-#[derive(Debug, Clone, Copy, PartialEq, Default)]
+lazy_static! {
+    static ref DIRAC_DICE: HashMap<u64, usize> = (1..=3)
+        .cartesian_product(1..=3)
+        .cartesian_product(1..=3)
+        .counts_by(|((a, b), c)| a + b + c);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Hash)]
 struct Player {
     score: u64,
     position: u64,
 }
 
-fn play<DI: Iterator<Item = u64>>(
-    mut dice: DI,
-    mut players: Vec<Player>,
-) -> (Vec<Player>, u64) {
-    let mut rolls = 0;
-
-    loop {
-        for player in &mut players {
-            let moves = dice.next().unwrap() + dice.next().unwrap() + dice.next().unwrap();
-            let last_pos = player.position;
-            player.position = ((player.position + moves - 1) % BOARD_SIZE) + 1;
-            player.score += player.position;
-            rolls += 3;
-
-            if player.score >= WINNING_SCORE{
-                return (players.iter().sorted_by_key(|p| p.score).rev().copied().collect_vec(), rolls)
-            }
-        }
+impl Player {
+    pub fn go_forward(&mut self, moves: u64) {
+        self.position = ((self.position + moves - 1) % BOARD_SIZE) + 1;
+        self.score += self.position;
     }
 }
 
@@ -39,20 +34,62 @@ fn parse_input(s: &str) -> impl Iterator<Item = Player> + '_ {
 
 fn part_one(s: &str) -> String {
     let input = parse_input(s);
-    let players = input.collect_vec();
-    let dice = (1..=100).cycle();
+    let mut players = input.collect_vec();
+    let mut dice = (1..=100).cycle();
+    let winning_score = 1_000;
 
-    let (players, rolls) = play(dice, players);
+    let mut rolls = 0;
+
+    'game: loop {
+        for player in &mut players {
+            let moves = dice.next().unwrap() + dice.next().unwrap() + dice.next().unwrap();
+            player.go_forward(moves);
+            rolls += 3;
+
+            if player.score >= winning_score {
+                break 'game;
+            }
+        }
+    }
+
+    players.sort_by_key(|p| p.score);
+    players.reverse();
+
     let output = players.last().unwrap().score * rolls;
 
     format!("{}", output)
 }
 
-fn part_two(s: &str) -> String {
-    let input = parse_input(s);
-    let players = input.collect_vec();
+#[memoize]
+fn play(p1: Player, p2: Player) -> (usize, usize) {
+    if p1.score >= 21 {
+        return (1, 0);
+    }
+    if p2.score >= 21 {
+        return (0, 2);
+    }
 
-    let output = 0;
+    let mut p1_total = 0;
+    let mut p2_total = 0;
+
+    for (moves, freq) in DIRAC_DICE.iter() {
+        let mut next = p1;
+        next.go_forward(*moves);
+
+        let (p2_wins, p1_wins) = play(p2, next);
+        p1_total += p1_wins * freq;
+        p2_total += p2_wins * freq;
+    }
+
+    (p1_total, p2_total)
+}
+
+fn part_two(s: &str) -> String {
+    let mut input = parse_input(s);
+
+    let totals = play(input.next().unwrap(), input.next().unwrap());
+
+    let output = totals.0.max(totals.1) / 2; // Uhhh... TODO: figure out why it's double lol
 
     format!("{}", output)
 }
