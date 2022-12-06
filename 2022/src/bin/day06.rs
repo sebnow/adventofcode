e44a27e443830dcac56c6944d5bc53cae8e4fd5c
Sebastian Nowicki <8548+sebnow@users.noreply.github.com>
2022-12-06 09:45:07 +0100

    2022-06: Solve part 2

--- 2022/src/bin/day06.rs
+++ 2022/src/bin/day06.rs
@@ -1,7 +1,7 @@
 use anyhow::{anyhow, Result};
 
-fn start_of_packet(s: &[char]) -> (&[char], usize) {
-    let (offset, sop) = s.windows(4).enumerate().find(|(_, window)| {
+fn start_of_pattern(s: &[char], count_distinct: usize) -> (&[char], usize) {
+    let (offset, sop) = s.windows(count_distinct).enumerate().find(|(_, window)| {
         let mut v = Vec::from(*window);
         v.sort();
         v.dedup();
@@ -9,7 +9,7 @@ fn start_of_packet(s: &[char]) -> (&[char], usize) {
         v.len() == window.len()
     }).unwrap();
 
-    (sop, offset + 4)
+    (sop, offset + count_distinct)
 }
 
 fn parse_input(s: &str) -> Result<Vec<char>> {
@@ -18,14 +18,16 @@ fn parse_input(s: &str) -> Result<Vec<char>> {
 
 fn part_one(s: &str) -> String {
     let input = parse_input(s).unwrap();
-    let (_, offset) = start_of_packet(&input);
+    let (_, offset) = start_of_pattern(&input, 4);
 
     offset.to_string()
 }
 
 fn part_two(s: &str) -> String {
     let input = parse_input(s).unwrap();
-    "".to_string()
+    let (_, offset) = start_of_pattern(&input, 14);
+
+    offset.to_string()
 }
 
 fn main() -> Result<()> {
@@ -46,5 +48,5 @@ mod test {
     test_example!(example_6_1_3, part_one, 6, 1, 3);
     test_example!(example_6_1_4, part_one, 6, 1, 4);
     test_example!(example_6_1_5, part_one, 6, 1, 5);
-    //test_example!(example_6_2, part_two, 6, 2, 1);
+    test_example!(example_6_2_1, part_two, 6, 2, 1);
 }
