ead7a774dd0575360233ea6858fcfa4e5bc2dda5
Sebastian Nowicki <8548+sebnow@users.noreply.github.com>
2022-12-08 17:11:57 +0100

    2022-08: Solve part 2

--- 2022/src/bin/day08.rs
+++ 2022/src/bin/day08.rs
@@ -50,9 +50,65 @@ fn part_one(s: &str) -> String {
         .to_string()
 }
 
+fn scenic_score(height_map: &Grid, p: &Point) -> usize {
+    let h = height_map.get(p).unwrap();
+    let is_blocking = |other_h: &u32| other_h >= h;
+
+    let mut left = 0;
+    for other_h in (0..p.x)
+        .rev()
+        .filter_map(|x| height_map.get(&Point::new(x, p.y)))
+    {
+        left += 1;
+        if is_blocking(other_h) {
+            break;
+        }
+    }
+
+    let mut right = 0;
+    for other_h in
+        (p.x + 1..height_map.rows() as i64).filter_map(|x| height_map.get(&Point::new(x, p.y)))
+    {
+        right += 1;
+        if is_blocking(other_h) {
+            break;
+        }
+    }
+
+    let mut up = 0;
+    for other_h in (0..p.y)
+        .rev()
+        .filter_map(|y| height_map.get(&Point::new(p.x, y)))
+    {
+        up += 1;
+        if is_blocking(other_h) {
+            break;
+        }
+    }
+
+    let mut down = 0;
+    for other_h in
+        (p.y + 1..height_map.cols() as i64).filter_map(|y| height_map.get(&Point::new(p.x, y)))
+    {
+        down += 1;
+        if is_blocking(other_h) {
+            break;
+        }
+    }
+
+    left * right * up * down
+}
+
 fn part_two(s: &str) -> String {
     let input = parse_input(s).unwrap();
-    "".to_string()
+    let height_map = input;
+
+    height_map
+        .iter()
+        .map(|(p, _)| scenic_score(&height_map, p))
+        .max()
+        .unwrap()
+        .to_string()
 }
 
 fn main() -> Result<()> {
@@ -69,5 +125,5 @@ mod test {
     use aocutil::test_example;
 
     test_example!(example_8_1, part_one, 8, 1, 1);
-    //test_example!(example_8_2, part_two, 8, 2, 1);
+    test_example!(example_8_2, part_two, 8, 2, 1);
 }
