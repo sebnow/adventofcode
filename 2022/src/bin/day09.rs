ccb8d9e5c7f2d9df5b8b3f20e580165e34d91b07
Sebastian Nowicki <8548+sebnow@users.noreply.github.com>
2022-12-09 19:57:51 +0100

    2022-09: Solve part 2

--- 2022/src/bin/day09.rs
+++ 2022/src/bin/day09.rs
@@ -5,34 +5,35 @@ use aocutil::{Point, Vector};
 
 const MAX_DISTANCE: i64 = 2;
 
-type Rope = (Point, Point);
+type Rope = Vec<Point>;
 
-fn step_rope((mut head, tail): Rope, step: Vector) -> Rope {
-    head += step;
+fn simulate(length: usize, motions: &[Vector]) -> usize {
+    let mut visited = HashSet::new();
+    let mut rope: Rope = Vec::with_capacity(length);
+    rope.resize_with(length, Default::default);
 
-    (
-        head,
-        tail + match (head.x - tail.x, head.y - tail.y) {
-            (dx, dy) if dx.abs() >= MAX_DISTANCE || dy.abs() >= MAX_DISTANCE => {
-                Vector::new(dx.signum(), dy.signum())
+    for motion in motions {
+        let target = rope[0] + *motion;
+
+        let step = Vector::new(motion.x.signum(), motion.y.signum());
+        while rope[0] != target {
+            rope[0] += step;
+
+            for idx in 1..rope.len() {
+                let (head, tail) = (rope[idx - 1], rope[idx]);
+                rope[idx] += match (head.x - tail.x, head.y - tail.y) {
+                    (dx, dy) if dx.abs() < MAX_DISTANCE && dy.abs() < MAX_DISTANCE => {
+                        Vector::zero()
+                    }
+                    (dx, dy) => Vector::new(dx.signum(), dy.signum()),
+                }
             }
-            _ => Vector::zero(),
-        },
-    )
-}
 
-fn move_rope(mut rope: Rope, m: Vector) -> Vec<Rope> {
-    let mut history = Vec::new();
-    let step = Vector::new(m.x.signum(), m.y.signum());
-
-    let target = rope.0 + m;
-    while rope.0 != target {
-        history.push(rope);
-        rope = step_rope(rope, step);
+            visited.insert(rope[rope.len() - 1]);
+        }
     }
 
-    history.push(rope);
-    history
+    visited.len()
 }
 
 fn parse_input(s: &str) -> Result<Vec<Vector>> {
@@ -53,25 +54,12 @@ fn parse_input(s: &str) -> Result<Vec<Vector>> {
 
 fn part_one(s: &str) -> String {
     let input = parse_input(s).unwrap();
-
-    input
-        .into_iter()
-        .fold(
-            (HashSet::<Point>::new(), (Point::zero(), Point::zero())),
-            |(mut visited, rope), m| {
-                let ropes = move_rope(rope, m);
-                visited.extend(ropes.iter().map(|r| r.1));
-                (visited, ropes[ropes.len() - 1])
-            },
-        )
-        .0
-        .len()
-        .to_string()
+    simulate(2, &input).to_string()
 }
 
 fn part_two(s: &str) -> String {
     let input = parse_input(s).unwrap();
-    "".to_string()
+    simulate(10, &input).to_string()
 }
 
 fn main() -> Result<()> {
@@ -87,6 +75,6 @@ mod test {
     use super::*;
     use aocutil::test_example;
 
-    test_example!(day09_example_1, part_one, 9, 1, 1);
-    test_example!(day09_example_2, part_two, 9, 2, 1);
+    test_example!(day09_1, part_one, 9, 1, 1);
+    test_example!(day09_2, part_two, 9, 2, 1);
 }
