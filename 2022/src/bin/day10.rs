a000936f4905f6a0d6b82de1ccc611e22c8df24f
Sebastian Nowicki <sebnow@gmail.com>
2022-12-10 10:00:11 +0100

    2022-10: Solve part 2

--- 2022/src/bin/day10.rs
+++ 2022/src/bin/day10.rs
@@ -1,8 +1,30 @@
 use anyhow::Result;
+use itertools::Itertools;
+
+const LIT: char = '#';
+const DARK: char = '.';
 
 enum Instr {
     Noop,
     AddX(i64),
+}
+
+fn process<I: Iterator<Item = Instr>>(instructions: I) -> Vec<i64> {
+    instructions.fold(vec![1], |mut cycles, instr| {
+        let x = cycles[cycles.len() - 1];
+
+        match instr {
+            Instr::Noop => {
+                cycles.push(x);
+            }
+            Instr::AddX(n) => {
+                cycles.push(x);
+                cycles.push(x + n);
+            }
+        }
+
+        cycles
+    })
 }
 
 fn parse_input(s: &str) -> Result<impl Iterator<Item = Instr> + '_> {
@@ -16,20 +38,7 @@ fn parse_input(s: &str) -> Result<impl Iterator<Item = Instr> + '_> {
 
 fn part_one(s: &str) -> String {
     let input = parse_input(s).unwrap();
-    let cycles = input
-        .fold((vec![1], 1), |(mut cycles, mut x), instr| match instr {
-            Instr::Noop => {
-                cycles.push(x);
-                (cycles, x)
-            }
-            Instr::AddX(n) => {
-                cycles.push(x);
-                x += n;
-                cycles.push(x);
-                (cycles, x)
-            }
-        })
-        .0;
+    let cycles = process(input);
 
     let signal_strength: i64 = 20 * cycles[19]
         + cycles
@@ -45,13 +54,34 @@ fn part_one(s: &str) -> String {
 
 fn part_two(s: &str) -> String {
     let input = parse_input(s).unwrap();
-    "".to_string()
+    let cycles = process(input);
+    let width: i64 = 40;
+
+    cycles
+        .iter()
+        .enumerate()
+        .take(width as usize * 6)
+        .chunks(width as usize)
+        .into_iter()
+        .map(|cycles| {
+            (0..width as i64)
+                .zip(cycles)
+                .map(|(crt, (_, sprite))| {
+                    if sprite - 1 <= crt && crt <= sprite + 1 {
+                        LIT
+                    } else {
+                        DARK
+                    }
+                })
+                .collect::<String>()
+        })
+        .join("\n")
 }
 
 fn main() -> Result<()> {
     let input = include_str!("../../input/day10.txt");
     println!("Part one: {}", part_one(input));
-    println!("Part two: {}", part_two(input));
+    println!("Part two:\n{}", part_two(input));
 
     Ok(())
 }
@@ -62,5 +92,5 @@ mod test {
     use aocutil::test_example;
 
     test_example!(day10_1, part_one, 10, 1, 1);
-    //test_example!(day10_2, part_two, 10, 2, 1);
+    test_example!(day10_2, part_two, 10, 2, 1);
 }
